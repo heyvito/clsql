77a75ba46df9ea96e77a7b78af45494f0fb9b6f3
Victor Gama <hey@vito.io>
2020-06-19 18:20:57 -0300

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clsql "0.1.1"
+(defproject clsql "0.1.2"
   :description "clsql provides a small abstraction for handling SQL and migrations"
   :url "https://github.com/heyvito/clsql"
   :license {:name "MIT"}
